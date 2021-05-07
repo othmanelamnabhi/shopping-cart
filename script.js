5d02d7c5285a82389a112f5ef77aefc94e3e72f3
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2021-05-07 01:52:08 +0000

    write function toadd product to cart

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Select items to be used for the project
 const cartButton = document.querySelector("#cart-button");
 const cartContent = document.querySelector("#cart-content");
-const productTemplate = document.querySelector("template");
+const productTemplate = document.querySelector("#product-template");
+const cartItemTemplate = document.querySelector("#cart-item-template");
 const productsGrid = document.querySelector("#products-grid");
+const cartItemsGrid = document.querySelector("#cart-items-grid");
+let productsArray = [];
 let requestURL = "./items.json";
 
 // Show or hide the modal when it is clicked
@@ -10,8 +13,46 @@ cartButton.addEventListener("click", showHideCartModal);
 function showHideCartModal() {
   cartContent.classList.toggle("invisible");
 }
-
+// Fetch and render products from JSON file
 fetchJSONData(requestURL);
+// Add event listener to all add to cart buttons
+document.addEventListener("click", renderCartItem);
+
+function renderCartItem(event) {
+  if (!event.target.matches("[data-product-id]")) return;
+  let clonedCartItemTemplate = cartItemTemplate.content.cloneNode(true);
+  // Add attributes to the HTML to make them easy to select and also to store data in case
+  // const productId = clonedTemplate.querySelector("[data-product-id]");
+  const cartItemMultiplier = clonedCartItemTemplate.querySelector(
+    "[data-cart-item-multiplier]"
+  );
+  const cartItemName = clonedCartItemTemplate.querySelector(
+    "[data-cart-item-name]"
+  );
+  const cartItemPrice = clonedCartItemTemplate.querySelector(
+    "[data-cart-item-price]"
+  );
+  const cartItemColor = clonedCartItemTemplate.querySelector(
+    "[data-cart-item-color]"
+  );
+
+  // Replace template text by text in JSON file
+  productsArray.forEach((product) => {
+    console.log(event.target);
+    if (event.target.dataset.productId == product.id) {
+      cartItemColor.src = `https://dummyimage.com/210x130/${product.imageColor}/${product.imageColor}`;
+      cartItemColor.dataset.cartItemColor = product.imageColor;
+      cartItemName.innerText = product.name;
+      cartItemName.dataset.cartItemName = product.name;
+      // Since the JSON file contains the price as an integer and in cents we divide by 100
+      cartItemPrice.innerText = `$${(product.priceCents / 100).toFixed(2)}`;
+      cartItemPrice.dataset.productPrice = product.priceCents;
+
+      //productId.dataset.productId = product.id;
+      cartItemsGrid.appendChild(clonedCartItemTemplate);
+    }
+  });
+}
 
 // Dynamically render products from JSON file by using template
 function renderProducts(productsArray) {
@@ -28,12 +69,15 @@ function renderProducts(productsArray) {
 
     // Replace template text by text in JSON file
     productColor.src = `https://dummyimage.com/420x260/${product.imageColor}/${product.imageColor}`;
+    productColor.dataset.productColor = product.imageColor;
     productCategory.innerText = product.category;
+    productCategory.dataset.productCategory = product.category;
     productName.innerText = product.name;
+    productName.dataset.productName = product.name;
     // Since the JSON file contains the price as an integer and in cents we divide by 100
     productPrice.innerText = `$${(product.priceCents / 100).toFixed(2)}`;
+    productPrice.dataset.productPrice = product.priceCents;
     productId.dataset.productId = product.id;
-    console.log(clonedTemplate);
     productsGrid.appendChild(clonedTemplate);
   });
 }
@@ -47,28 +91,15 @@ function fetchJSONData(jsonLink) {
 
   request.onload = function () {
     const products = request.response;
+    returnProductArray(products);
     renderProducts(products);
   };
 }
+// Grab the request response and push it into an array
+function returnProductArray(requestResponse) {
+  productsArray.push(...requestResponse);
+}
 
-// <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-//   <div class="block relative h-48 rounded overflow-hidden">
-//     <img
-//       alt="ecommerce"
-//       class="object-cover object-center w-full h-full block"
-//       src="https://dummyimage.com/420x260/F00/F00"
-//     />
-//   </div>
-//   <div class="mt-4 flex items-end justify-between">
-//     <div>
-//       <h3 class="text-gray-500 text-xs tracking-widest title-font uppercase mb-1">
-//         Primary Color
-//       </h3>
-//       <h2 class="text-gray-900 title-font text-lg font-medium">Red</h2>
-//       <p class="mt-1">$16.00</p>
-//     </div>
-//     <button class="text-white py-2 px-4 text-xl bg-blue-500 rounded hover:bg-blue-700">
-//       Add To Cart
-//     </button>
-//   </div>
-// </div>;
+// Try new approach
+// Store the array in the JSON file in a variable
+// Modify the renderProducts function, delete the attributes, keep only ID
